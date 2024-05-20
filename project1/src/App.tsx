ac45cb60adf2ca5d11edb0388e39b54a607da086
Himanshu Kathania <76757042+Himanshu-Kathania@users.noreply.github.com>
2024-05-20 13:10:09 +0530

    ReactProject added

---
+++ project1/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import AppBar from "./components/AppBar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <AppBar />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
